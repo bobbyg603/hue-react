aaaea56e8bb595b14eb7a30f8e7cf8ff1fd795e7
Bobby Galli <bobbyg603@gmail.com>
2021-01-19 12:50:07 -0500

    Added tests for Lights component.

--- src/components/Lights/Lights.test.tsx
+++ src/components/Lights/Lights.test.tsx
@@ -1,69 +1,202 @@
 import { act, render, screen } from '@testing-library/react';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { configure, mount } from 'enzyme';
 import React from 'react';
-import { BrowserRouter } from '../Groups/node_modules/react-router-dom';
-import { RouteableBulb as Light } from '../Bulb/Bulb';
+import { BrowserRouter as Router } from 'react-router-dom';
+import * as util from '../../utils/debounce';
 import Lights from './Lights';
 
+configure({ adapter: new Adapter() });
+
 describe('<Lights />', () => {
+  let id;
   let lightsService;
   let fakeLights;
 
   beforeEach(async () => {
-    lightsService = {
-      getLights: jest.fn()
-    };
-    fakeLights = createFakeLights();
-    lightsService.getLights.mockResolvedValue(fakeLights);
+    fakeLights = [
+      {
+        id: '1',
+        name: 'Living Room 1',
+        state: {
+          on: true,
+          brightness: 254,
+          color: {
+            r: 128,
+            g: 128,
+            b: 128
+          }
+        }
+      },
+      {
+        id: '2',
+        name: 'Living Room 2',
+        state: {
+          on: false,
+          brightness: 0,
+          color: {
+            r: 0,
+            g: 255,
+            b: 0
+          }
+        }
+      }
+    ];
 
-    await act(async () => { 
-      render(
-        <BrowserRouter>
-          <Lights lightsService={lightsService} />
-        </BrowserRouter>
-      );
-    });
+    lightsService = {
+      getLights: jest.fn(),
+      getLightById: jest.fn(),
+      setName: jest.fn(),
+      setState: jest.fn()
+    };
+    lightsService.getLights.mockResolvedValue(fakeLights);
+    lightsService.getLightById.mockResolvedValue(fakeLights[0]);
+    id = fakeLights[0].id;
+
+    (util.debounce as any) = jest.fn().mockImplementation((func, wait) => func);
   });
 
-  test('it should mount', async () => {
+  it('it should mount', async () => {
+    await act(async () => {
+      render(
+        <Router>
+          <Lights lightsService={lightsService} />
+        </Router>
+      );
+    });
+
     const lights = screen.getByTestId('Lights');
 
     expect(lights).toBeInTheDocument();
   });
 
-  test('it should get lights from LightsService', () => {
-    expect(lightsService.getLights).toHaveBeenCalled();
+  describe('when no id is specified', () => {
+    beforeEach(async () => {
+      await act(async () => {
+        render(
+          <Router>
+            <Lights lightsService={lightsService} />
+          </Router>
+        );
+      });
+    });
+
+    it('it should call getLights from LightsService', () => {
+      expect(lightsService.getLights).toHaveBeenCalled();
+    });
+
+    it('it should map response from getLights to Bulbs', () => {
+      const bulbs = screen.getAllByTestId('Bulb');
+
+      expect(bulbs.length).toEqual(fakeLights.length);
+    });
   });
 
-  test('it should map response from LightsService to Bulbs', () => {
-    const bulbs = screen.getAllByTestId('Bulb');
+  describe('when an id is specified', () => {
+    beforeEach(async () => {
+      await act(async () => {
+        render(
+          <Router>
+            <Lights id={id} lightsService={lightsService} />
+          </Router>
+        );
+      });
+    });
 
-    expect(bulbs.length).toEqual(Object.keys(fakeLights).length);
+    it('should call getLightById with id', () => {
+      expect(lightsService.getLightById).toHaveBeenCalledWith(id);
+    });
+
+    it('should map result of getLightById to bulb', () => {
+      const bulbs = screen.getAllByTestId('Bulb');
+
+      expect(bulbs.length).toEqual(1);
+    });
+  });
+
+  describe('when onNameChange emits', () => {
+    let name;
+    let wrapper;
+
+    beforeEach(async () => {
+      name = 'Living Room Light 3';
+      await act(async () => {
+        wrapper = mount(
+          <Router>
+            <Lights id={id} lightsService={lightsService} />
+          </Router>
+        );
+      });
+    });
+
+    it('should call debounce with func and wait', () => {
+      expect(util.debounce).toHaveBeenCalledWith(
+        expect.any(Function),
+        100
+      );
+    });
+
+    it('should call setName with id and name', async () => {
+      const bulb = wrapper.update().find('Bulb');
+
+      await act(async () => bulb.prop('onNameChange')(name));
+
+      expect(lightsService.setName).toHaveBeenCalledWith(id, name);
+    });
+
+    it('should cause getLightsById to be called again to sync state', async () => {
+      const bulb = wrapper.update().find('Bulb');
+
+      await act(async () => bulb.prop('onNameChange')(name));
+
+      expect(lightsService.getLightById).toHaveBeenCalledTimes(2);
+    });
+  });
+
+  describe('when onStateChange emits', () => {
+    let state;
+    let wrapper;
+
+    beforeEach(async () => {
+      state = {
+        on: false,
+        brightness: 123,
+        color: {
+          r: 101,
+          g: 101,
+          b: 101
+        }
+      };
+      await act(async () => {
+        wrapper = mount(
+          <Router>
+            <Lights id={id} lightsService={lightsService} />
+          </Router>
+        );
+      });
+    });
+
+    it('should call debounce with func and wait', () => {
+      expect(util.debounce).toHaveBeenCalledWith(
+        expect.any(Function),
+        100
+      );
+    });
+
+    it('should call setName with id and name', async () => {
+      const bulb = wrapper.update().find('Bulb');
+
+      await act(async () => bulb.prop('onStateChange')(state));
+
+      expect(lightsService.setState).toHaveBeenCalledWith(id, state);
+    });
+
+    it('should cause getLightsById to be called again to sync state', async () => {
+      const bulb = wrapper.update().find('Bulb');
+
+      await act(async () => bulb.prop('onStateChange')(state));
+
+      expect(lightsService.getLightById).toHaveBeenCalledTimes(2);
+    });
   });
 });
-
-function createFakeLights(): Array<Light> {
-  return [
-    {
-      id: '1',
-      name: 'Living Room 1',
-      brightness: 254,
-      on: true,
-      color: {
-        r: 128,
-        g: 128,
-        b: 128
-      }
-    },
-    {
-      id: '2',
-      name: 'Living Room 2',
-      brightness: 0,
-      on: false,
-      color: {
-        r: 0,
-        g: 255,
-        b: 0
-      }
-    }
-  ];
-}
--- src/components/Lights/Lights.tsx
+++ src/components/Lights/Lights.tsx
@@ -1,9 +1,9 @@
-import AwesomeDebouncePromise from 'awesome-debounce-promise';
 import React, { useEffect, useState } from 'react';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import { Link } from 'react-router-dom';
 import { LightsService } from '../../services/Lights/lights-service';
+import { debounce } from '../../utils/debounce';
 import Bulb, { Light, LightSize, LightState } from '../Bulb/Bulb';
 import './Lights.css';
 
@@ -16,8 +16,8 @@ const Lights: React.FC<LightsProps> = (props: LightsProps) => {
   const [lights, setLights] = useState([] as Array<Light>);
   const [refresh, setRefresh] = useState(0);
 
-  const debouncedSetName = AwesomeDebouncePromise((id, name) => props.lightsService.setName(id, name), 100);
-  const debouncedSetState = AwesomeDebouncePromise((id, state) => props.lightsService.setState(id, state), 100);
+  const debouncedSetName = debounce((id, name) => props.lightsService.setName(id, name), 100);
+  const debouncedSetState = debounce((id, state) => props.lightsService.setState(id, state), 100);
 
   useEffect(() => {
     if (!props.id) {
